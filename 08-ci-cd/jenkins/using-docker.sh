d3b3ec7631229083d14c040743a9eec9a1f7bd84
Gaurav Agarwal <algogrit@gmail.com>
2023-03-21 09:22:52 +0530

    Updating the final command.

--- 08-ci-cd/jenkins/using-docker.sh
+++ 08-ci-cd/jenkins/using-docker.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -itd -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
+# docker run -itd -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts
+
+docker run -itd --rm -p 8080:8080 -p 50000:50000 agarwalconsulting/jenkins-with-go
